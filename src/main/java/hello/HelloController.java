10d25364bc518bd2e4d608f0c8c45a5a2b449f31
Aluno <Aluno@cipucrs.pucrs.br>
2018-08-23 17:00:49 -0300

    hehe

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,13 +2,17 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.joda.time.LocalTime;
+import org.joda.time.LocalDate;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/data")
     public String index() {
-        return "Greetings from Spring Boot!";
+	LocalDate currentDate = new LocalDate(); 
+	LocalTime currentTime = new LocalTime();
+        return  currentDate.toString() + " " +  currentTime.getHourOfDay() + "h" + currentTime.getMinuteOfHour();
     }
 
 }
